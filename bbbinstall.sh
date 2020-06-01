b8b8deb47ad5483514d5faa7517b3a8f65607e2c
Ashish Amar <ashishamar1999@gmail.com>
2020-06-01 21:25:14 +0530

    Delete bbbinstall.sh

--- bbbinstall.sh
+++
@@ -1,5 +0,0 @@
-# contains commands to install bigbluebutton
-
-wget https://ubuntu.bigbluebutton.org/repo/bigbluebutton.asc -O- | sudo apt-key add -
-echo "deb https://ubuntu.bigbluebutton.org/xenial-22/ bigbluebutton-xenial main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
-sudo apt-get update
